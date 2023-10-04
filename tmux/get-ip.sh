7778af180a87b601fea6fcf868955b33a760b8a5
0xMoonrise <josephxcharly@gmail.com>
2023-10-04 13:08:48 -0700

    Modified the get-ip.sh script.

--- tmux/get-ip.sh
+++ tmux/get-ip.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 INET=''
-
-if [[ -f /sys/class/net/tun0/operstate ]]; then
-    INET='tun0'
-elif [[ $(< /sys/class/net/eth0/operstate) == "up" ]]; then
-    INET='eth0'
+name=('tun0' 'eth0' 'wlan0')
+if [[ -f /sys/class/net/${name[1]}/operstate ]]; then
+    INET=${name[1]}
+elif [[ $(< /sys/class/net/${name[2]}/operstate) == "up" ]]; then
+    INET=${name[2]}
 else
-    INET='wlan0'
+    INET=${name[3]}
 fi
 
 echo $(ip -4 addr show $INET | grep -oP "(?<=inet\s)\d+(\.\d+){3}")
