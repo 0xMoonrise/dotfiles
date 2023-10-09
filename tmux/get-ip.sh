8dd334c568da53e3150927323cb81d21587050c6
0xMoonrise <josephxcharly@gmail.com>
2023-10-08 18:33:04 -0700

    Rewritten get-ip script

--- tmux/get-ip.sh
+++ tmux/get-ip.sh
@@ -1,16 +1,5 @@
 #/bin/bash
 
-inets=$(ip addr | awk '/^[0-9]+: (.*):/ {print $2}')
-
-if [[ $inets =~ tun[0-9]+  && $(< /sys/class/net/${BASH_REMATCH[0]}/operstate) == "up" ]]; then
-        inet="${BASH_REMATCH[0]}"
-elif [[ $inets =~ eth[0-9]+|enp.*[0-9]+  && $(< /sys/class/net/${BASH_REMATCH[0]}/operstate) == "up" ]]; then
-        inet="${BASH_REMATCH[0]}"
-elif [[ $inets =~ wlan[0-9]+  && $(< /sys/class/net/${BASH_REMATCH[0]}/operstate) == "up" ]]; then
-        inet="${BASH_REMATCH[0]}"
-else
-        echo [no inet or ip]
-        exit 1
-fi
-
-echo $(ip -4 addr show $inet | grep -oP "(?<=inet\s)\d+(\.\d+){3}")
+ip -o -4 addr show | awk '/tun[0-9]+/ {print $4; exit}
+			  /eth[0-9]+|enp.*[0-9]+/ {print $4; exit}
+			  /wlan[0-9]+/ {print $4; exit}'
