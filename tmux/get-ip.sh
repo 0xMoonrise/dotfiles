e6518eeaf59aba1c406a6f55171d6d29687814db
0xMoonrise <josephxcharly@gmail.com>
2023-10-14 20:28:33 -0700

    Added shrink ip format

--- tmux/get-ip.sh
+++ tmux/get-ip.sh
@@ -2,4 +2,4 @@
 
 ip -o -4 addr show | awk '/tun[0-9]+/ {print $4; exit}
 			  /eth[0-9]+|enp.*[0-9]+/ {print $4; exit}
-			  /wlan[0-9]+/ {print $4; exit}'
+			  /wlan[0-9]+/ {print $4; exit}' | cut -d '/' -f1
