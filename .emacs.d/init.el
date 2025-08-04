9415347b39c996b2f5eb1981161dd484bddc7c2e
0xMoonrise <josephxcharly@gmail.com>
2025-08-03 23:29:19 -0700

    chore: reorder package archives and theme setup; add nongnu; cleanup faces

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1,4 +1,4 @@
-;;; init.el --- Main entry for Emacs config -*- lexical-binding: t; -*-
+;; init.el --- Main entry for Emacs config -*- lexical-binding: t; -*-
 ;;; Commentary:
 ;;; Emacs configuration.
 ;;; Code:
@@ -7,9 +7,9 @@
 (load (expand-file-name "custom.el" user-emacs-directory))
 (load (expand-file-name "keybindings.el" user-emacs-directory))
 
-(setq package-archives
-      '(("gnu" . "https://elpa.gnu.org/packages/")
-        ("melpa" . "https://melpa.org/packages/")))
+(setq package-archives '(("melpa" . "https://melpa.org/packages/")
+                         ("gnu" . "https://elpa.gnu.org/packages/")
+                         ("nongnu" . "https://elpa.nongnu.org/nongnu/")))
 
 (unless package--initialized
   (package-initialize))
@@ -29,13 +29,23 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-   '(company flycheck go-mode lsp-mode lsp-treemacs lsp-ui python-mode
-             sublime-themes)))
+   '(cape consult corfu corfu-popupinfo corfu-terminal flycheck
+          go-mode lsp-treemacs lsp-ui marginalia orderless python-mode
+          pyvenv seq sublime-themes vertico)))
+
+(add-to-list 'custom-theme-load-path "~/.emacs.d/themes")
+
+(use-package sublime-themes
+  :ensure t
+  :config
+  (load-theme 'spolsky t)
+  (set-face-attribute 'default nil :background "black" :foreground "white")
+  (set-face-background 'vertical-border "gray20"))
+
+;;; init.el ends here
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
-
-;;; init.el ends here
+ '(default ((t (:background nil)))))
