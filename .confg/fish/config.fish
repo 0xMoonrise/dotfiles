4346de876470de9e4ff4c0b4e99ea949995e14a9
0xMoonrise <josephxcharly@gmail.com>
2025-12-02 03:23:15 -0700

    Improve fish config: QoL aliases, env vars, utility functions and tmux auto-attach

--- .confg/fish/config.fish
+++ .confg/fish/config.fish
@@ -1,10 +1,58 @@
+# Main fish settings
 set fish_greeting ""
+
 function fish_prompt
     set dir (basename $PWD)
     if test $PWD = $HOME
         set dir "~"
     end
-	echo [(whoami)@(hostname) $dir]
-	echo "\$ "
+        echo ["$USER"@$hostname $dir]
+        echo "\$ "
 end
-uv generate-shell-completion fish | source
+
+# quality of life aliases
+alias set-inet "tmux set-environment INET"
+
+# useful variables
+set -gx TERM xterm-256color
+set -gx INET eth0
+set -gx PATH $PATH /usr/local/go/bin
+set -gx GOPATH $HOME/go
+set -xg GOOS linux
+set -xg GOARCH arm64
+
+# functions and utility code
+function download_pdf
+    wget $argv -P /opt/lectures/
+end
+
+function upload_archive
+ curl -v -X POST http://localhost:8080/upload -F "file=@$argv"
+end
+
+function set_inet
+    tmux set-environment -g INET "$argv"
+end
+
+if status is-interactive
+and not set -q TMUX
+    tmux attach -t base || tmux new -s base
+end
+
+function envsource
+  set -f envfile "$argv"
+  if not test -f "$envfile"
+    echo "Unable to load $envfile"
+    return 1
+  end
+  while read line
+    if not string match -qr '^#|^$' "$line"
+      set item (string split -m 1 '=' $line)
+      set -gx $item[1] $item[2]
+      echo "Exported key $item[1]"
+    end
+  end < "$envfile"
+end
+
+# Generated for envman. Do not edit.
+test -s ~/.config/envman/load.fish; and source ~/.config/envman/load.fish
