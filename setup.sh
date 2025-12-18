8aaaa87a04c572ccc8a83a8c9026936010f041c7
0xMoonrise <josephxcharly@gmail.com>
2025-12-18 03:58:10 -0700

    Refactor setup script to centralize symlink creation with set_link_directory function

--- setup.sh
+++ setup.sh
@@ -1,15 +1,22 @@
 #!/usr/bin/env bash
 
+function set_link_directory()
+{
+  echo "[INFO] Setting up '$1' directory"
+  find $1 -type f -print0 | while IFS= read -r -d $'\0' FILE; do
+    echo "$FILE"
+    ln -fn "$FILE" "$HOME/$FILE"
+  done  
+}
+
 echo "[INFO] Setting up $HOME"
 for FILE in * .[^.]*; do
-    [[ $FILE != .* ]] && continue
-    [[ $FILE =~ ^(\.emacs\.d|\.git|\.config)$ ]] && continue
-    echo "$FILE"
-    ln -fn "$FILE" "$HOME/$FILE"
+  [[ $FILE != .* ]] && continue
+  [[ $FILE =~ ^(\.emacs\.d|\.git|\.config)$ ]] && continue
+  echo "$FILE"
+  ln -fn "$FILE" "$HOME/$FILE"
 done
 
-echo "[INFO] Setting up '.config' directory"
-find .config/ -type f -print0 | while IFS= read -r -d $'\0' FILE; do
-    echo "$FILE"
-    ln -fn "$FILE" "$HOME/$FILE"
-done
+set_link_directory ".config/"
+
+set_link_directory ".emacs.d/"
